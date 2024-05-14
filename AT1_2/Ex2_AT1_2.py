760ba5d205fd48868bb31356f7c589e8cce9e4cd
Mari <122611455+MariRangel04@users.noreply.github.com>
2024-05-14 14:41:46 -0300

    Create Ex2_AT1_2.py

---
+++ AT1_2/Ex2_AT1_2.py
@@ -0,0 +1,21 @@
+print("Caixa:")
+for i in range(5):
+        print("*" * 5)
+        
+print("Oval:")
+print(" *** ")
+print("*   *")
+print("*   *")
+print("*   *")
+print(" *** ")
+
+print("Seta:")
+print("  * ")
+print(" *** ")
+print("*****")
+print(" ** ")
+print(" ** ")
+print(" **")
+
+
+
