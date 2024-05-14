28822666efde5f79961dccbd1e08c1e0b4e27243
Mari <122611455+MariRangel04@users.noreply.github.com>
2024-05-14 14:26:53 -0300

    Create Ex1_AT1_2.py

---
+++ AT1_2/Ex1_AT1_2.py
@@ -0,0 +1,7 @@
+num1 = int(input('Digite um valor:'))
+num2 = int(input('Digite outro valor:'))
+
+print('Soma:', num1 + num2)
+print('Produto:', num1 * num2)
+print('Diferença:', num1 - num2)
+print('Divisão', num1/ num2)
