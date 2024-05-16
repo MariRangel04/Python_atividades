88a1acf1153aa62111d6f08fa010c45b5a50350f
Mari <122611455+MariRangel04@users.noreply.github.com>
2024-05-16 15:46:11 -0300

    Create geral.py

---
+++ geral/geral.py
@@ -0,0 +1,32 @@
+##converter para inteiro
+print('int(3.14)=', int(3.14))
+
+#converter inteiro para real
+print('float(5)=', float(5))
+
+##inteiro e real resulta em real
+print('5.0/2+3 =', 5.0/2+3)
+
+##numeros complexos
+c=3+4j
+print('c=', c)
+print('parte real:', c.real)
+print('parte imaginaria:', c.imag)
+print('conjugado:', c.conjugate())
+
+##tamanho
+print(len('Alexandre o melhor'))
+
+f = 'fusca'
+print(' O '+ f +' esta na rua ')
+
+nome = 'alexandre'
+print(nome.upper())
+print(nome.capitalize()) 
+
+nome = input('Digite seu nome:')
+idade = input('Digite sua idade:')
+print('Seu nome e {} e tem {} anos'.format(nome,idade))
+
+a = 'ale'
+print(f'Alo {a}')
