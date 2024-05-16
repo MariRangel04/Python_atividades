1c8a20653e66879bfb5f44d3bb1130e67cdb46a8
Mari <122611455+MariRangel04@users.noreply.github.com>
2024-05-16 15:43:36 -0300

    Update geral.py

--- geral.py
+++ geral.py
@@ -25,9 +25,9 @@ nome = 'alexandre'
 print(nome.upper())
 print(nome.capitalize()) 
 
-##nome = input('Digite seu nome:')
-##idade = input('Digite sua idade:')
-##print('Seu nome e {} e tem {} anos'.format(nome,idade))
+nome = input('Digite seu nome:')
+idade = input('Digite sua idade:')
+print('Seu nome e {} e tem {} anos'.format(nome,idade))
 
 a = 'ale'
 print(f'Alo {a}')
