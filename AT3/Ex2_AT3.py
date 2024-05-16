9ac755c0ca4f5f0df1cbbea68c657a63f2bad17a
Mari <122611455+MariRangel04@users.noreply.github.com>
2024-05-16 15:40:21 -0300

    Update Ex2_AT3.py

--- AT3/Ex2_AT3.py
+++ AT3/Ex2_AT3.py
@@ -1,1 +1,33 @@
 ##jogo da forca
+print(""" 
+      
+      *****************
+      **Jogo da Forca**            
+      *****************
+""")
+
+palavra_reservada= 'Banana'
+acertou=False
+enforcou=False
+erros=0
+
+palavra= ['_','_','_','_','_','_']
+
+print(palavra)
+
+chute = input('Chute uma letra:')
+
+for i, letra in enumerate(palavra_reservada):
+    if chute.upper() == letra.upper():
+        palavra[i]=chute
+        
+acertou = not '_' in palavra
+        
+print(palavra)
+
+if acertou == True:
+    print("""
+         ***************
+         **Você venceu**
+         ***************    
+          """)
