913c0fd770db81b16c1bfcb3b0c22c21f2d99426
Mari <122611455+MariRangel04@users.noreply.github.com>
2024-05-16 15:13:37 -0300

    Create Ex11AT1_2.py

---
+++ AT1_2/Ex11AT1_2.py
@@ -0,0 +1,17 @@
+##ver se o numero é primo ou não melhorado
+numero = int(input('Digite um valor:'))
+
+aux = numero
+contador = 0 
+
+while aux>0:
+    if numero%aux == 0:
+        contador +=1
+    aux-=1
+    if contador>2: break
+    
+if contador == 2:
+    print('É primo!')
+else: 
+    print('Não é primo')
+    
