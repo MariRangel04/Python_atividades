462fc2e135fa8c0f5cda39490de3b00edcf43813
Mari <122611455+MariRangel04@users.noreply.github.com>
2024-05-20 12:03:05 -0300

    Create Ex4_AT4.py

---
+++ AT4/Ex4_AT4.py
@@ -0,0 +1,12 @@
+## dicionario info
+informacoes = {
+    "first_name": "João",
+    "last_name": "Silva",
+    "age": 30,
+    "city": "São Paulo"
+}
+
+print("Primeiro nome:", informacoes["first_name"])
+print("Sobrenome:", informacoes["last_name"])
+print("Idade:", informacoes["age"])
+print("Cidade:", informacoes["city"])
