6f36b9fefed91388fbc7d477e338301fa6f6f193
Mari <122611455+MariRangel04@users.noreply.github.com>
2024-05-20 11:04:03 -0300

    Create Ex1_AT4.py

---
+++ AT4/Ex1_AT4.py
@@ -0,0 +1,11 @@
+##qtd caracteres
+frase = input('Digite uma frase:')
+contagem = {}
+
+for char in frase:
+    if char in contagem:
+        contagem[char] += 1
+    else:
+        contagem[char] = 1
+
+print(contagem)
