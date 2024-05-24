c24836298e0d54e8246063ecf350fe8d236c6b0d
Mari <122611455+MariRangel04@users.noreply.github.com>
2024-05-24 20:50:31 -0300

    Create Settings.py

---
+++ Aulas/Settings.py
@@ -0,0 +1,14 @@
+class settings:
+
+    """"
+    Uma classe para armazenar todas as invasoes alienigenas
+    """
+
+def __init__(self):
+    """
+    inicializa as configurações
+    """
+    
+    self.screen_height = 800
+    self.screen_width = 1200
+    self.bg_color = (230,230,230)
