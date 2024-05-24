6543c1b7cead9a01527c1c0011fc615fb9691157
Mari <122611455+MariRangel04@users.noreply.github.com>
2024-05-24 20:49:39 -0300

    Create main.py

---
+++ Aulas/main.py
@@ -0,0 +1,24 @@
+##pygame
+import pygame
+import sys 
+import Settings 
+import Ship
+
+
+def run_game():
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption('Alien Ivasion')
+    screen.fill(ai_settings.bg_color)
+    ship =Ship(screen)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit
+
+        ship.blitme()
+        pygame.display.flip()
+
+run_game()
