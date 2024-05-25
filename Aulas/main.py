7a87ef23e341f5ad21ae28cf4e6fbc15a08d4c8a
Mari <122611455+MariRangel04@users.noreply.github.com>
2024-05-24 21:18:38 -0300

    Update main.py

--- Aulas/main.py
+++ Aulas/main.py
@@ -1,24 +1,21 @@
-##pygame
 import pygame
 import sys 
 import Settings 
 import Ship
+import game_function as gf
 
 
 def run_game():
     pygame.init()
-    ai_settings = Settings()
+    ai_settings = Settings.settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Ivasion')
-    screen.fill(ai_settings.bg_color)
     ship =Ship(screen)
 
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings,screen,ship)
 
-        ship.blitme()
-        pygame.display.flip()
 
 run_game()
