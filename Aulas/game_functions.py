b34214aa228dac5f0610c035764a0d101268e0f5
Mari <122611455+MariRangel04@users.noreply.github.com>
2024-05-24 21:20:50 -0300

    Create game_functions.py

---
+++ Aulas/game_functions.py
@@ -0,0 +1,27 @@
+import pygame
+import sys
+
+def check_events(ship):
+    """
+    responde eventos tecla e mouse
+    """
+
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                 if event.key == pygame.K_RIGHT:
+                      ship.moving_right = True
+                      ship.rect.centerx +=1
+            elif event.type == pygame.KEYUP:
+                 if event.key == pygame.K_RIGHT:
+                      ship.moving_right = False
+
+def update_screen(ai_settings, screen, ship):
+     """
+     atualiza imagens, e alterna para nova tela
+     """
+     screen.fill(ai_settings.bg_color)
+     pygame.display.flip()
+     ship.blitme()
